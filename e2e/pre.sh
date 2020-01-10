29439081eff2251c661da8ced0e51aebf1200f50
Micah Huber <micah@fastmail.us>
2020-01-10 15:22:46 -0500

    use full path in call to apk

--- e2e/pre.sh
+++ e2e/pre.sh
@@ -9,6 +9,6 @@ else
     echo "CI_SHA1: $CI_SHA1"
 fi
 
-apk add go=1.12.12-r0
+/sbin/apk add go=1.12.12-r0
 /usr/bin/go build -o ./rbac-lookup
 docker cp ./rbac-lookup e2e-command-runner:rbac-lookup
